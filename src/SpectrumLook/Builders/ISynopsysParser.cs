a9e562efffb6b392d567a0723fe72408ddce2cca
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-26 18:11:18 -0700

    Fix typos

--- src/SpectrumLook/Builders/ISynopsysParser.cs
+++ src/SpectrumLook/Builders/ISynopsysParser.cs
@@ -12,10 +12,10 @@ namespace SpectrumLook.Builders
     public interface ISynopsysParser
     {
         /// <summary>
-        /// Each time this function is called is should return a string array corrisponding to the current row of data.
+        /// Each time this function is called is should return a string array corresponding to the current row of data.
         /// The first time this function is called it should return the headers for each column.
-        /// The headers if concatinated with "_s" means that this is the connection between the synopsis file and the
-        /// experiment file if concatinated with "_p"  this mean that this is the Peptide column.
+        /// The headers if concatenated with "_s" means that this is the connection between the synopsis file and the
+        /// experiment file if concatenated with "_p"  this mean that this is the Peptide column.
         /// If there are no more rows to read the parser should return an empty string array.
         /// </summary>
         /// <returns></returns>
